64f51314f0f7354a82275707bad57fca30bda32a
NAEK2K <naek2k@outlook.com>
2022-06-20 23:10:11 -0400

    more comments

--- api/main.py
+++ api/main.py
@@ -12,10 +12,13 @@ from database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(title="Music Archive")
 
 
 def get_db():
+    """
+    DB dependency.
+    """
     db = SessionLocal()
     try:
         yield db
@@ -23,16 +26,22 @@ def get_db():
         db.close()
 
 
-@app.post("/users/", response_model=schema.User)
+@app.post("/users/", response_model=schema.User, tags=["users"])
 def create_user(user: schema.UserCreate, db: Session = Depends(get_db)):
+    """
+    Route to create a new user.
+    """
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered.")
     return crud.create_user(db=db, user=user)
 
 
-@app.post("/bands/", response_model=schema.Band)
+@app.post("/bands/", response_model=schema.Band, tags=["bands"])
 def create_band(band: schema.BandBase, db: Session = Depends(get_db)):
+    """
+    Route to create a new band.
+    """
     db_band = crud.get_band_by_name(db, name=band.name)
     if db_band:
         raise HTTPException(status_code=400, detail="Band already exists.")
